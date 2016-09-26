bb37c57cd5ca5ad3f9d4200f3816e3b07a9f5061
maga-senthil <magalakshmi.s@gmail.com>
2016-09-25 21:11:56 -0500

    initial

---
+++ app.js
@@ -0,0 +1,677 @@
+var dataObject = [{ 
+	"id":"272822514",
+	"firstName": "Billy",
+	"lastName": "Bob",
+	"gender": "male",
+	"dob": "1/18/1949",
+	"height": 71,
+	"weight": 175,
+	"eyeColor": "brown",
+	"occupation": "programmer",
+	"parents": [],
+	"currentSpouse": 401222887 
+},{
+	"id":"401222887",
+	"firstName": "Uma",
+	"lastName": "Bob",
+	"gender": "female",
+	"dob": "4/1/1947",
+	"height": 65,
+	"weight": 162,
+	"eyeColor": "brown",
+	"occupation": "assistant",
+	"parents": [],
+	"currentSpouse": 272822514 
+},{
+	"id":"409574486",
+	"firstName": "Michael",
+	"lastName": "Walkens",
+	"gender": "male",
+	"dob": "5/9/1951",
+	"height": 76,
+	"weight": 250,
+	"eyeColor": "brown",
+	"occupation": "landscaper",
+	"parents": [],
+	"currentSpouse": 260451248 
+},{
+	"id":"260451248",
+	"firstName": "Jon",
+	"lastName": "Walkens",
+	"gender": "male",
+	"dob": "9/6/1945",
+	"height": 62,
+	"weight": 115,
+	"eyeColor": "brown",
+	"occupation": "assistant",
+	"parents": [],
+	"currentSpouse": 409574486 
+},{
+	"id":"629807187",
+	"firstName": "Jack",
+	"lastName": "Pafoy",
+	"gender": "male",
+	"dob": "3/16/1938",
+	"height": 70,
+	"weight": 207,
+	"eyeColor": "black",
+	"occupation": "nurse",
+	"parents": [],
+	"currentSpouse": 464142841 
+},{
+	"id":"464142841",
+	"firstName": "Jen",
+	"lastName": "Pafoy",
+	"gender": "female",
+	"dob": "4/10/1940",
+	"height": 72,
+	"weight": 256,
+	"eyeColor": "black",
+	"occupation": "student",
+	"parents": [],
+	"currentSpouse": 629807187 
+},{
+	"id":"982411429",
+	"firstName": "Mister",
+	"lastName": "Potatoo",
+	"gender": "male",
+	"dob": "12/18/1952",
+	"height": 66,
+	"weight": 170,
+	"eyeColor": "hazel",
+	"occupation": "architect",
+	"parents": [],
+	"currentSpouse": 595767575 
+},{
+	"id":"595767575",
+	"firstName": "Missuz",
+	"lastName": "Potatoo",
+	"gender": "female",
+	"dob": "10/28/1948",
+	"height": 59,
+	"weight": 137,
+	"eyeColor": "blue",
+	"occupation": "architect",
+	"parents": [],
+	"currentSpouse": 982411429 
+},{
+	"id":"693243224",
+	"firstName": "Joy",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "4/20/1939",
+	"height": 69,
+	"weight": 199,
+	"eyeColor": "hazel",
+	"occupation": "doctor",
+	"parents": [],
+	"currentSpouse": 693243224 
+},{
+	"id":"888201200", 
+	"firstName": "Mader",
+	"lastName": "Madden",
+	"gender": "male",
+	"dob": "5/6/1937",
+	"height": 76,
+	"weight": 205,
+	"eyeColor": "black",
+	"occupation": "landscaper",
+	"parents": [],
+	"currentSpouse": 888201200 
+},{
+	"id":"878013758",
+	"firstName": "Jill",
+	"lastName": "Pafoy",
+	"gender": "Bob",
+	"dob": "2/8/1972",
+	"height": 74,
+	"weight": 118,
+	"eyeColor": "brown",
+	"occupation": "programmer",
+	"parents": [401222887,401222887],
+	"currentSpouse": 294874671 
+},{
+	"id":"951747547",
+	"firstName": "Ralph",
+	"lastName": "Bob",
+	"gender": "male",
+	"dob": "12/23/1969",
+	"height": 66,
+	"weight": 179,
+	"eyeColor": "blue",
+	"occupation": "nurse",
+	"parents": [401222887,401222887],
+	"currentSpouse": 159819275 
+},{
+	"id":"159819275",
+	"firstName": "Jasmine",
+	"lastName": "Bob",
+	"gender": "female",
+	"dob": "12/18/1969",
+	"height": 58,
+	"weight": 156,
+	"eyeColor": "blue",
+	"occupation": "assistant",
+	"parents": [409574486,260451248],
+	"currentSpouse": 951747547 
+},{
+	"id":"348457184",
+	"firstName": "Annie",
+	"lastName": "Pafoy",
+	"gender": "female",
+	"dob": "11/4/1970",
+	"height": 62,
+	"weight": 235,
+	"eyeColor": "hazel",
+	"occupation": "landscaper",
+	"parents": [629807187,464142841],
+	"currentSpouse": null 
+},{
+	"id":"294874671",
+	"firstName": "Dave",
+	"lastName": "Pafoy",
+	"gender": "male",
+	"dob": "8/5/1967",
+	"height": 61,
+	"weight": 112,
+	"eyeColor": "green",
+	"occupation": "doctor",
+	"parents": [629807187,464142841],
+	"currentSpouse": 878013758 
+},{
+	"id":"931247228",
+	"firstName": "Amii",
+	"lastName": "Pafoy",
+	"gender": "female",
+	"dob": "3/13/1963",
+	"height": 74,
+	"weight": 184,
+	"eyeColor": "brown",
+	"occupation": "landscaper",
+	"parents": [629807187,464142841],
+	"currentSpouse": null 
+},{
+	"id":"822843554",
+	"firstName": "Regina",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "7/26/1959",
+	"height": 71,
+	"weight": 249,
+	"eyeColor": "brown",
+	"occupation": "nurse",
+	"parents": [693243224, 888201200],
+	"currentSpouse": null 
+},{
+	"id":"819168108",
+	"firstName": "Hana",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "10/7/1953",
+	"height": 70,
+	"weight": 187,
+	"eyeColor": "brown",
+	"occupation": "politician",
+	"parents": [693243224, 888201200],
+	"currentSpouse": null 
+},{
+	"id":"969837479",
+	"firstName": "Eloise",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "12/11/1961",
+	"height": 63,
+	"weight": 241,
+	"eyeColor": "brown",
+	"occupation": "assistant",
+	"parents": [693243224, 888201200],
+	"currentSpouse": null 
+},{
+	"id":"313207561",
+	"firstName": "Mattias",
+	"lastName": "Madden",
+	"gender": "male",
+	"dob": "2/19/1966",
+	"height": 70,
+	"weight": 110,
+	"eyeColor": "blue",
+	"occupation": "assistant",
+	"parents": [693243224, 888201200],
+	"currentSpouse": 313997561 
+},{
+	"id":"313997561",
+	"firstName": "Ellen",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "2/19/1970",
+	"height": 67,
+	"weight": 100,
+	"eyeColor": "blue",
+	"occupation": "doctor",
+	"parents": [],
+	"currentSpouse": 313207561 
+},{
+	"id":"313998000",
+	"firstName": "Joey",
+	"lastName": "Madden",
+	"gender": "female",
+	"dob": "2/02/1987",
+	"height": 67,
+	"weight": 100,
+	"eyeColor": "blue",
+	"occupation": "doctor",
+	"parents": [313207561,313997561],
+	"currentSpouse": null 
+}
+];
+
+
+function search(){
+	var value = prompt("Enter Height,Weight,Occupation and EyeColor.Please type your search terms, separated by commas:");
+	var foo = seachByMultipleCriteria(value,dataObject);
+	responder(foo);
+}
+function initSearch(){
+	var fname=prompt("Enter first name:");
+	var lname=prompt("Enter last name");
+	if(isName(fname,lname)){
+	var person=getPersonInfo(fname, lname,dataObject);
+	responder(person);
+	var personId=getID(fname,dataObject);
+	var decendants=getDescendants(personId,dataObject);
+	var decendantResult= displayDescendants(decendants);
+	responder(decendantResult);
+	var spouse = getSpouse(person,dataObject);
+	var children = getChildren(person,dataObject);
+	if(children){
+	 children.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+	}
+	var parents = getParents(person,dataObject);
+	if(parents){
+	 parents.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+	}
+	var grandparents = getGrandParents(parents,dataObject);
+	if(grandparents){
+	 grandparents.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+	}
+	var siblings = getSiblings(parents,person,dataObject);
+	if(siblings){
+	 siblings.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+	}
+	var grandchildren = getGrandChild(children,dataObject);
+	if(grandchildren){
+	 grandchildren.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+	}
+	var auntUncle = getAuntandUncle(grandparents,person,dataObject);
+	if(auntUncle){
+	auntUncle.sort(function(a,b){
+     return new Date(a.dob) - new Date(b.dob);
+    });
+    }
+	var nieceAndNephew = getNieceandNephew(siblings,dataObject);
+	var immediateFamilyMembers = immediateFamily(person,parents,siblings,spouse,children);
+	responder(immediateFamilyMembers);
+	var resultArray = nextOfKin(person,spouse,children,parents,grandparents,siblings,grandchildren,auntUncle,nieceAndNephew);
+	responder(resultArray);
+
+	var value = prompt("Enter age")
+	var r=findPersonOnAge(value,dataObject);
+	responder(r);
+	var value = prompt("Enter Height or EyeColor or Weight or Occupation or gender")
+	var foo = seachByCriteria(value,dataObject);
+	responder(foo);
+	search();
+	}
+}
+function isName(firstname,lastname){
+	for(var i = 0; i < dataObject.length; i++){
+		if(dataObject[i].firstName.toLowerCase() == firstname.toLowerCase() && dataObject[i].lastName.toLowerCase() == lastname.toLowerCase()){
+			return true;
+		}
+	}
+	alert("Enter correct name");
+	initSearch();
+	return false;
+}
+function seachByMultipleCriteria(myString,dataObj){
+	try{
+	var myArray = new Array();
+	myArray = myString.split(',');
+    var filterarray  = [];
+     var filteredData = dataObj.filter(function(a){return (a.height) == parseInt(myArray[0]) && (a.weight) == parseInt(myArray[1]) && (a.occupation) == myArray[2] && (a.eyeColor) == myArray[3];});
+	return filteredData;
+		}
+	catch(error){
+		alert("Enter Values in proper Order");
+		search();
+	}	
+}
+   
+function displayDescendants(decendants,oneArray=[]){
+	oneArray.push("DECENDANTS  :");
+	if(decendants){
+	for(var i = 0; i < decendants.length;i++){
+	oneArray.push(decendants[i].firstName+" "+decendants[i].lastName);
+       }
+	}
+	return oneArray;
+}
+function nextOfKin(person,spouse,children,parents,grandparents,siblings,grandchildren,auntUncle,nieceAndNephew,newArray =[]){
+	newArray.push("NEXT OF KIN :")
+	newArray.push("PERSON  :");
+	newArray.push(person.firstName+" "+person.lastName);
+	if(spouse){
+	for(var i = 0 ; i < spouse.length ;i++){
+		newArray.push("SPOUSE  :")
+		newArray.push("  "+spouse[i].firstName+" "+spouse[i].lastName);
+	}
+	}
+	if(children){
+	if(children.length > 0){
+	newArray.push("CHILDREN  :");
+	for(var i = 0 ; i < children.length ;i++){
+		newArray.push("  "+children[i].firstName+" "+children[i].lastName);
+	}
+    }
+	}
+    if(parents){
+    if(parents.length > 0){
+	newArray.push("PARENTS  :");
+	for(var i = 0 ; i < parents.length ;i++){
+		newArray.push("  "+parents[i].firstName+" "+parents[i].lastName);
+	}
+	}
+	}
+	if(grandparents){
+	if(grandparents.length > 0){
+	newArray.push("GRANDPARENTS  :");	
+	for(var i = 0 ; i < grandparents.length ;i++){
+		newArray.push("  "+grandparents[i].firstName+" "+grandparents[i].lastName);
+	}
+	}
+	}
+	if(siblings){
+	if(siblings.length > 0){
+	newArray.push("SIBLINGS   :");	
+	for(var i = 0 ; i < siblings.length ;i++){
+		newArray.push("  "+siblings[i].firstName+" "+siblings[i].lastName);
+	}
+	}
+	}
+	if(grandchildren){
+	if(grandchildren.length > 0){
+	newArray.push("GRANDCHILDREN   :");	
+	for(var i = 0 ; i < grandchildren.length ;i++){
+		newArray.push("  "+grandchildren[i].firstName+" "+grandchildren[i].lastName);
+	}
+	}
+	}
+	if(auntUncle){
+	if(auntUncle.length > 0){
+	newArray.push("AUNT/UNCLE  :");
+    for(var i = 0 ; i < auntUncle.length ;i++){
+		newArray.push("  "+auntUncle[i].firstName+" "+auntUncle[i].lastName);
+	}
+	}
+	}
+	if(nieceAndNephew){
+	if(nieceAndNephew.length > 0){	
+    for(var i = 0 ; i < nieceAndNephew.length ;i++){
+    	newArray.push("NIECE/NEPHEW  :");
+		newArray.push("  "+nieceAndNephew[i].firstName+" "+nieceAndNephew[i].lastName);
+	}
+    }
+	}
+	return newArray;
+}
+function immediateFamily(person,parents,siblings,spouse,children,familyArray=[]){
+	familyArray.push("IMMEDIATE FAMILY :")
+	familyArray.push("PERSON  :");
+	familyArray.push(person.firstName+" "+person.lastName);
+    if(parents){
+    if(parents.length > 0){
+	familyArray.push("PARENTS  :");
+	for(var i = 0 ; i < parents.length ;i++){
+	familyArray.push("  "+parents[i].firstName+" "+parents[i].lastName);
+	}
+	}
+	}
+	if(siblings){
+	if(siblings.length > 0){
+	familyArray.push("SIBLINGS   :");	
+	for(var i = 0 ; i < siblings.length ;i++){
+		familyArray.push("  "+siblings[i].firstName+" "+siblings[i].lastName);
+	}
+	}
+	}
+	if(spouse){
+	for(var i = 0 ; i < spouse.length ;i++){
+		familyArray.push("SPOUSE  :")
+		familyArray.push("  "+spouse[i].firstName+" "+spouse[i].lastName);
+	}
+	}
+	if(children){
+	if(children.length > 0){
+	familyArray.push("CHILDREN  :");
+	for(var i = 0 ; i < children.length ;i++){
+		familyArray.push("  "+children[i].firstName+" "+children[i].lastName);
+	}
+    }
+	}
+	return familyArray;
+}
+function responder(result)
+{
+	alert(JSON.stringify(result,null,"\n"));
+}
+function getPersonInfo(firstname,lastname,personObj){
+	for(var key in personObj){
+		if(personObj.hasOwnProperty(key)){
+			if(personObj[key].firstName.toLowerCase()==firstname.toLowerCase()&& personObj[key].lastName.toLowerCase()==lastname.toLowerCase()){
+				return personObj[key];
+			}
+		}
+	}
+}
+function getID(firstname,personObj){
+	for(var key in personObj){
+		if(personObj.hasOwnProperty(key)){
+			if(personObj[key].firstName.toLowerCase()==firstname.toLowerCase()){
+				return personObj[key].id;
+			}
+		}
+	}
+	return null;
+}
+function getDescendants(id,personObj, personDescendent = []){
+	for(var key in personObj){
+		if(personObj.hasOwnProperty(key)){
+			if(parentMatchesChild(id, personObj[key])){
+				personDescendent.push(personObj[key]);
+			    getDescendants(personObj[key].id,personObj, personDescendent);
+			}
+	    }
+    }
+    return personDescendent;
+}
+function parentMatchesChild(parentID, childObj) {
+	for (var i = 0; i < childObj.parents.length; i++) {
+		if (childObj.parents[i] == parentID) {
+			return true;
+		}
+	}
+	return false;
+} 
+function getParents(person,dataObj){
+	try{
+		var parent=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				if(dataObj[key].id==person.parents[0] ||dataObj[key].id==person.parents[1]){
+					parent.push(dataObj[key]);
+				}
+			}
+		}
+		return parent;
+	}
+    catch(error){
+	alert("No parents");
+    }
+}
+function getGrandParents(parent,dataObj){
+	try{
+        var grandparent=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				if(dataObj[key].id==parent[0].parents[0] ||dataObj[key].id==parent[0].parents[1]){
+					grandparent.push(dataObj[key]);
+				}
+			}
+		}
+		return grandparent;
+	}
+    catch(error){
+	alert("No grandparents");
+    }
+}
+function getGrandChild(children,dataObj)
+{
+	try{
+		var grandchildren=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				if(parentMatchesChild(children.id,dataObj[key])){
+					grandchildren.push(dataObj[key]);
+				}
+			}
+		}
+		return grandchildren;
+	}
+    catch(error){
+	alert("No children");
+    }
+}
+function getSpouse(person,dataObj){
+	try{
+		var spouse=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				if(dataObj[key].id==person.currentSpouse){
+					spouse.push(dataObj[key]);
+				}
+			}
+		}
+		return spouse;
+	}
+    catch(error){
+	alert("No spouse");
+    }
+}
+function getChildren(person,dataObj){
+	try{
+		var children=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				if(parentMatchesChild(person.id,dataObj[key])){
+					children.push(dataObj[key]);
+				}
+			}
+		}
+		return children;
+	}
+    catch(error){
+	alert("No children");
+    }
+}
+function getSiblings(parent,person,dataObj){
+	try{
+		var siblingList=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+					if(parentMatchesChild(parent[0].id,dataObj[key])){
+						if(dataObj[key] != person){
+					siblingList.push(dataObj[key]);
+				    }
+				}
+				
+			}
+		}
+		 return siblingList;
+	}	
+   catch(error){
+   alert("No siblings");
+	}
+}
+function getAuntandUncle(grandparents,person,dataObj){
+    try{
+		var auntAndUncleList=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+					if(parentMatchesChild(grandparents[0].id,dataObj[key]) || parentMatchesChild(grandparents[1].id,dataObj[key])){
+						if(dataObj[key].id != person.parents[0]){
+							auntAndUncleList.push(dataObj[key]);
+					}
+				}	
+			}
+		}
+		 return auntAndUncleList;
+	}	
+   catch(error){
+	alert("No AuntAndUncle");
+    }
+}
+function getNieceandNephew(siblings,dataObj){
+	try{
+		var niece=[];
+		for(var key in dataObj){
+			if(dataObj.hasOwnProperty(key)){
+				for(var value in siblings){
+				    if(parentMatchesChild(siblings[value].id,dataObj[key])){
+					niece.push(dataObj[key]);
+				    }
+			    }
+			}
+		}
+		return niece;
+	}
+    catch(error){
+	alert("No NieceandNephew");
+	}
+}
+function seachByCriteria(name,dataObj){
+	var newList=[];
+	for(var key in dataObj){
+		for(var value in dataObj[key]){
+			if(dataObj[key][value]==name){
+				newList.push(dataObj[key].firstName+" "+dataObj[key].lastName);
+		    }
+	    }
+	}
+	return newList;
+}
+function findPersonOnAge(age,dataObj){
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    var currentMonth = currentDate.getMonth()+1;
+    var currentDay = currentDate.getDate();
+    var year = currentYear-age;
+    var startDate = currentMonth+"/"+currentDay+"/"+(year-1);
+    var endDate = currentMonth +"/"+(currentDay+1)+"/"+year;
+
+    var filteredData = dataObj.filter(function(a){return new Date(a.dob) > new Date(startDate) && new Date(a.dob) < new Date(endDate);});
+	return filteredData;
+}
+
+initSearch();
+//search();
+
